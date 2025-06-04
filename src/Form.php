ee77cc001b4d5e657984d670c1a20e9f680d8abb
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-04 12:11:20 +0200

    feat: Add stub of custom config form

---
+++ src/Form.php
@@ -0,0 +1,15 @@
+<?php
+namespace Horde\Passwd\Config;
+use Horde_Form;
+use Horde_Config_Form;
+use Horde;
+class Form extends Horde_Config_Form
+{    
+    public function __construct($vars, $app = 'passwd')
+    {
+        // custom content first
+        // TODO:
+        // Load xmlconfig
+        parent::__construct($vars, $app);
+    }
+}
