5b356c60b46538532b4cc82eef794f6ce1efe9e8
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-28 17:22:42 +0100

    chore(release): bump version to 6.0.0-beta1
    Release version 6.0.0-beta1
    
    fix: Use extension_loaded check as loadExtension is a removed PHP5 feature
    refactor(routes): migrate to builder pattern with smartmobile BC
    fix: Double encoding of URL
    Merge pull request #6 from horde/feat/responsive-mobile-first
    feat: Show app specific icon in mobile/responsive mode
    Merge pull request #5 from horde/feat/responsive-mobile-first
    feat(passwd): add smartmobile/responsive scaffolding
    fix: Shield against passing null values to horde/view components
    chore: Update gitignore file

--- lib/Application.php
+++ lib/Application.php
@@ -48,7 +48,7 @@ class Passwd_Application extends Horde_Registry_Application
     /**
      * The version of passwd as shown in the admin view
      */
-    public $version = '6.0.0-alpha4';
+    public $version = '6.0.0-beta1';
 
     /**
      * Supported features.
